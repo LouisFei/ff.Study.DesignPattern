5d2cd82f51a747b4cc0f4b06054b90b43f4ea36d
lqf <lqf12847@ly.com>
2016-01-04 09:56:28 +0800

    new baby

---
+++ ff.Study.DesignPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ff.Study.DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
