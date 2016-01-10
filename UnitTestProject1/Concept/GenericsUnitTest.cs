3f18dd129b66cdeb4bc3e7a4223a6dcf42445946
Louis.Lu <luqingfei@126.com>
2016-01-10 16:08:30 +0800

    RawGenericFactory

---
+++ UnitTestProject1/Concept/GenericsUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using ff.Study.DesignPattern.Concept.Generics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestProject1.Concept
+{
+    [TestClass]
+    public class GenericsUnitTest
+    {
+        interface IProduct { }
+        class ConcreteProduct : IProduct { }
+
+        [TestMethod]
+        public void RawGenericFactoryTest()
+        {
+            string typeName = typeof(ConcreteProduct).AssemblyQualifiedName;
+            IProduct product = RawGenericFactory.Create<IProduct>(typeName);
+
+            Assert.IsNotNull(product);
+            Assert.AreEqual<string>(typeName, product.GetType().AssemblyQualifiedName);
+        }
+    }
+}
---
+++ ff.Study.DesignPattern/Concept/Generics/RawGenericFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ff.Study.DesignPattern.Concept.Generics
+{
+    public static class RawGenericFactory
+    {
+        /// <summary>
+        /// 动态生成实例
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="typeName">
+        /// TopNameSpace.SubNameSpace.ContainingClass, MyAssembly
+        /// ContainingClass, MyAssembly, Version=1.3.0.0, PublicKeyToken=b17a5c561934e089
+        /// </param>
+        /// <returns></returns>
+        public static T Create<T>(string typeName)
+        {
+            return (T)Activator.CreateInstance(Type.GetType(typeName));
+        }
+    }
+}
