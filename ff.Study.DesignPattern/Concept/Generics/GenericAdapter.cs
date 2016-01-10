11ed7625372d7798d0803cb2c648123831e966f9
Louis.Lu <luqingfei@126.com>
2016-01-10 16:46:03 +0800

    Generic Adapter

---
+++ ff.Study.DesignPattern/Concept/Generics/GenericAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ff.Study.DesignPattern.Concept.Generics
+{
+    public interface ITarget { void Request(); }
+    public interface IAdaptee { void SpecifiedRequest(); }
+
+    public abstract class GenericAdapterBase<T> : ITarget where T : IAdaptee, new()
+    {
+        public virtual void Request()
+        {
+            new T().SpecifiedRequest();
+        }
+    }
+}
+
+/*
+ Generics 在结构型和行为型模式中的作用主要还是提高算法的适用性。
+ 例如：Adapter完成的是接口的转换，从更抽象的角度看，就是一个Type Cast（类型转换），
+ 所以用泛型可以定义出一个普适的IGenericAdapter。
+ */
