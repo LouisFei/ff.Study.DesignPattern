fad6cd79aeca058f5b5a0912ce458390d85cc93e
Louis.Lu <luqingfei@126.com>
2016-01-11 22:11:16 +0800

    Indexer

---
+++ UnitTestProject1/Concept/IndexerUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using ff.Study.DesignPattern.Concept.Indexer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestProject1.Concept
+{
+    [TestClass]
+    public class IndexerUnitTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            SingleColumnCollection c = new SingleColumnCollection();
+            Assert.AreEqual<string>("china", c[0]);
+            Assert.AreEqual<int>(2, c["ch"].Length); //命中china和chile两项
+            Assert.AreEqual<string>("china", c["ch"][0]);
+        }
+    }
+}
---
+++ ff.Study.DesignPattern/Concept/Indexer/SingleColumnCollection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ff.Study.DesignPattern.Concept.Indexer
+{
+    public class SingleColumnCollection
+    {
+        private static string[] countries = new string[] { "china", "chile", "uk" };
+
+        public string this[int index] { get { return countries[index]; } }
+
+        public string[] this[string name]
+        {
+            get
+            {
+                if(countries == null || countries.Length <= 0)
+                {
+                    return null;
+                }
+                else
+                {
+                    return Array.FindAll<string>(countries,
+                         delegate(string candicate)
+                         {
+                             return candicate.StartsWith(name);
+                         }
+                     );
+                }
+            }
+        }
+    }
+}
