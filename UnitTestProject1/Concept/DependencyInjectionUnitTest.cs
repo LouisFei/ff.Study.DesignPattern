cac385c90e60fe52ea0255df19e5b981ceec8ca6
Louis.Lu <luqingfei@126.com>
2016-01-17 22:36:39 +0800

    依赖注入

---
+++ UnitTestProject1/Concept/DependencyInjectionUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using ff.Study.DesignPattern.Concept.DependencyInjection.Example3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1.Concept
+{
+    [TestClass]
+    public class DependencyInjectionUnitTest
+    {
+        [TestMethod]
+        public void ConstructorDiTest()
+        {
+            ITimeProvider timeProvider = new Assembler().Create<ITimeProvider>();
+            Assert.IsNotNull(timeProvider); //确认可以正常获得抽象类型实例
+
+            ClientUseConstructorDi client = new ClientUseConstructorDi(timeProvider); //在构造函数中注入
+        }
+
+        [TestMethod]
+        public void SetterDiTest()
+        {
+            ITimeProvider timeProvider = new Assembler().Create<ITimeProvider>();
+            Assert.IsNotNull(timeProvider); //确认可以正常获得抽象类型实例
+
+            ClientUseSetterDi client = new ClientUseSetterDi();
+            client.TimeProvider = timeProvider; //通过Setter实现注入
+        }
+
+        [TestMethod]
+        public void InterfaceDiTest()
+        {
+            ITimeProvider timeProvider = new Assembler().Create<ITimeProvider>();
+            Assert.IsNotNull(timeProvider); //确认可以正常获得抽象类型实例
+
+            IObjectWithTimeProvider client = new ClientUseInterfaceDi();
+            client.TimeProvider = timeProvider; //通过接口实现注入
+        }
+
+        [TestMethod]
+        public void AttributeDiTest()
+        {
+            ClientUseAttribute client = new ClientUseAttribute();
+            Assert.IsTrue(client.GetYear() > 0);
+        }
+    }
+}
