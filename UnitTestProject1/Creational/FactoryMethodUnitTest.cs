f6d1c8aea80d31a07acfc89cda1901e294a85896
Louis.Lu <luqingfei@126.com>
2016-01-27 23:22:39 +0800

    FactoryMethod's simple factory

---
+++ UnitTestProject1/Creational/FactoryMethodUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ff.Study.DesignPattern.Creational.FactoryMethod.Example1;
+
+namespace UnitTestProject1.Creational
+{
+    [TestClass]
+    public class FactoryMethodUnitTest
+    {
+        [TestMethod]
+        public void Example1Test()
+        {
+            Factory factory = new Factory();
+            IProduct product = factory.Create();
+            Assert.AreEqual<Type>(product.GetType(), typeof(ConcreteProductA));
+        }
+
+        /// <summary>
+        /// 说明静态工厂可以根据提供的目标类型枚举变量选择需要实例化的类型
+        /// </summary>
+        [TestMethod]
+        public void StaticFactoryTest()
+        {
+            IProduct product = ProductFactory.Create(Category.B);
+            Assert.IsNotNull(product);
+            Assert.AreEqual<Type>(typeof(ConcreteProductB), product.GetType());
+        }
+    }
+}
---
+++ ff.Study.DesignPattern/Creational/FactoryMethod/Example1/code.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ff.Study.DesignPattern.Creational.FactoryMethod.Example1
+{
+    public interface IProduct { }
+    public class ConcreteProductA : IProduct { }
+
+    public class ConcreteProductB : IProduct { }
+
+    public class Factory
+    {
+        public IProduct Create()
+        {
+            // 工厂决定到底实例化哪个子类
+            return new ConcreteProductA();
+        }
+    }
+
+    public enum Category
+    {
+        A,
+        B
+    }
+
+    public static class ProductFactory
+    {
+        public static IProduct Create(Category category)
+        {
+            switch (category)
+            {
+                case Category.A:
+                    return new ConcreteProductA();
+                case Category.B:
+                    return new ConcreteProductB();
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+}
+
+/*
+ 通过IProduct隔离了客户程序与具体ConcreteProductX的依赖关系，在客户程序视野内根本就没有ConcreteProcutX。
+ 即使ConcreteProductX增加、删除方法或属性，也无妨大局，只要按照要求实现了IProduct就可以，Client无须关心ConcreteProductX的变化（确切地说它也关心不着，因为看不到）。
+ */
