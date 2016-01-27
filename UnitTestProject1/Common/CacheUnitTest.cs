c932822efbaea8a86b588c5fb851f3168aa520d2
Louis.Lu <luqingfei@126.com>
2016-01-27 22:20:53 +0800

    一个轻量的内存Cache

---
+++ UnitTestProject1/Common/CacheUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ff.Study.DesignPattern.Common;
+
+namespace UnitTestProject1.Common
+{
+    [TestClass]
+    public class CacheUnitTest
+    {
+        [TestMethod]
+        public void GenericCacheTest()
+        {
+            var cacheHelper = new GenericCache<string, string>();
+            cacheHelper.Add("hello", "world");
+
+            Assert.AreEqual<int>(1, cacheHelper.Count);
+            Assert.AreEqual<bool>(true, cacheHelper.ContainsKey("hello"));
+            Assert.AreEqual<bool>(false, cacheHelper.ContainsKey("hi"));
+
+            var val = "";
+            cacheHelper.TryGetValue("hello", out val);
+            Assert.AreEqual<string>("world", val);
+
+            cacheHelper.Clear();
+            Assert.AreEqual<int>(0, cacheHelper.Count);
+        }
+    }
+}
