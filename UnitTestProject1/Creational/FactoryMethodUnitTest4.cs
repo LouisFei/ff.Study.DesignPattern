99d875915f8dcf7f4a7de4d200915a607a60198c
Louis.Lu <luqingfei@126.com>
2016-03-26 00:47:55 +0800

    委托工厂

---
+++ UnitTestProject1/Creational/FactoryMethodUnitTest4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ff.Study.DesignPattern.Creational.FactoryMethod.GenericFactory;
+using ff.Study.DesignPattern.Creational.FactoryMethod.DelegateFactory;
+
+namespace UnitTestProject1.Creational
+{
+    [TestClass]
+    public class FactoryMethodUnitTest4
+    {
+        /// <summary>
+        /// 委托工厂
+        /// </summary>
+        [TestMethod]
+        public void TestDelegateFactory()
+        {
+            IFactory<CalculateHandler> factory = new CalculateHandlerFactory();
+            CalculateHandler handler = factory.Create();
+
+            Assert.AreEqual<int>(1 + 2 + 3, handler(1, 2, 3));
+        }
+
+        /*
+         P94
+         */
+    }
+}
---
+++ ff.Study.DesignPattern/Creational/FactoryMethod/DelegateFactory/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ff.Study.DesignPattern.Creational.FactoryMethod.GenericFactory;
+
+namespace ff.Study.DesignPattern.Creational.FactoryMethod.DelegateFactory
+{
+    //委托本质上就是对具体执行方法的抽象，它相当于 product 的角色
+    public delegate int CalculateHandler (params int[] items);
+
+    public class Calculator
+    {
+        //这个方法相当于 Delegate Factory 看到的 Concrete Product，具体产品
+        public int Add(params int[] items)
+        {
+            int result = 0;
+            foreach (var item in items)
+            {
+                result += item;
+            }
+
+            return result;
+        }
+    }
+
+    //具体工厂  Concrete Factory
+    public class CalculateHandlerFactory : IFactory<CalculateHandler>
+    {
+        public CalculateHandler Create()
+        {
+            return new Calculator().Add;
+        }
+    }
+}
